6e8b21f368731da68f625bcfb4785443cd9e945a
Naveen Tulsi <tulsi.naveen@gmail.com>
2019-04-25 21:31:33 +0530

    changes some typos

--- src/app/shared/services/login/login.service.ts
+++ src/app/shared/services/login/login.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Userdata } from '../../interfaces/userdata';
-import { catchError } from "rxjs/operators";
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Observable, Observer } from "rxjs";
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +12,14 @@ export class LoginService {
   constructor(private _httpClient: HttpClient) { }
 
   public login<T>(loginData: Userdata): Observable<T> {
-    let serverURL = "";
+    let serverURL = '';
     let requestBody;
     try {
       requestBody = JSON.stringify(loginData);
     } catch (e) {
       console.log(' to parse data');
       return Observable.create(function(fallback: Observer<string>) {
-        fallback.next(" to Login now.");
+        fallback.next(' to Login now.');
       })
     }
     return this._httpClient.post(serverURL, requestBody, {
@@ -27,8 +27,8 @@ export class LoginService {
     }).pipe(
       catchError((err: HttpErrorResponse, caught: Observable<any>) => {
         return Observable.create(function(fallback: Observer<string>) {
-          fallback.next("Unable to Login now.");
-        })
+          fallback.next('Unable to Login now.');
+        });
       })
     );
   }
